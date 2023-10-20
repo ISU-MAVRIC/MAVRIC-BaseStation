91f6a010e3744b1dea94ecfe1db0b59cc44e5ede
Nathan Logston <natestorms416@gmail.com>
2023-10-20 13:59:09 -0500

    minor spelling mistake :(

--- raspi-scripts/fuse_setup.bash
+++ raspi-scripts/fuse_setup.bash
@@ -3,7 +3,7 @@
 # This script will install and setup fuse, which is required to run AppPackages
 
 echo "installing fuse for AppPackage"
-suod apt-get install fuse libfuse2
+sudo apt-get install fuse libfuse2
 sudo addgroup fuse
 sudo adduser $USER fuse
 sudo modprobe -v fuse
