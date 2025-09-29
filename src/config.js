cb13d14bad3196044c87e353cb9b2c13f3df17a0
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-09-29 00:48:04 +0000

    Add comment with rover's IP, when running on the rover

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // Configuration file for MAVRIC BaseStation
 // ROSBridge WebSocket URL - change this value as needed
 const ROSBRIDGE_URL = "ws://localhost:9090";
+// USE [ ws://192.168.1.10:9090 ] for the rover
 
 export default {
   ROSBRIDGE_URL,
