22211f22f117e661c05f874a65c1e2bdf1bbbe8d
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-11-08 05:11:25 -0600

    test

--- src/components/DriveController.svelte
+++ src/components/DriveController.svelte
@@ -530,8 +530,7 @@
   }}
   on:RB={RB}
   on:LB={LB}
-  on:DPadLeft_PRESS={LuminometerStraight}
-  on:DPadRight_PRESS={LuminometerDump}
+  on:DPadLeft_PRESS={LuminometerStraight} on:DPadRight_PRESS={LuminometerDump}
   on:DPadUp={DrillUp}
   on:DPadDown={DrillDown}
   on:A={CacheMove}
